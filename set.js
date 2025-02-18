6661129515b15aa0bb05b05cd558e35d49d62b26
hxgdsjzuyt1234hg <jaihacku@gmail.com>
2025-02-18 21:46:50 +0530

    Update set.js

--- set.js
+++ set.js
@@ -2,7 +2,7 @@ module.exports = {
     ChannelUsername: "@rainonetime",
     githubToken: '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',
     githubRepo: 'ZephyrineNasution/databasex',
-    ownerId: '7308812889',
+    ownerId: '7430156268',
     githubResellerFilePath: 'resellers.json',
     githubFilePath: 'trashissue.json'
 };
