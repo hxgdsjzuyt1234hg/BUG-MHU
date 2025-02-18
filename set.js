46fcab7ed824e417df7ba0cee280a17093da0bdc
hxgdsjzuyt1234hg <jaihacku@gmail.com>
2025-02-18 20:33:17 +0530

    Add files via upload

---
+++ set.js
@@ -0,0 +1,9 @@
+module.exports = {
+    ChannelUsername: "@rainonetime",
+    githubToken: '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',
+    githubRepo: 'ZephyrineNasution/databasex',
+    ownerId: '7308812889',
+    githubResellerFilePath: 'resellers.json',
+    githubFilePath: 'trashissue.json'
+};
+
