750e33a3a33ce56ea3ff3b1304345f99473c5583
hxgdsjzuyt1234hg <jaihacku@gmail.com>
2025-02-18 20:34:06 +0530

    Add files via upload

---
+++ Configuration/Setting.js
@@ -0,0 +1,10 @@
+/*
+         Created by Zephyrine 
+        Telegram : @rainoneday
+   All Right Reversed By Zephyrine
+*/
+
+module.exports = {
+    TokenS: "7776286968:AAEv-aNlU21h8E2uEiHiAsj8mY7PEVqA2ks",
+    admins: ["7430156268"]
+};
